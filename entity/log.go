69373c7674231fb815fddb774f478aa12d2ded31
viveknathani <viveknathani2402@gmail.com>
2022-01-05 14:50:08 +0530

    chore: setup entity using schema

---
+++ entity/log.go
@@ -0,0 +1,12 @@
+package model
+
+type Log struct {
+	Id        string  `json:"id"`
+	UserId    string  `json:"userId"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Activity  string  `json:"activity"`
+	StartTime int64   `json:"startTime"`
+	EndTime   int64   `json:"endTime"`
+	Notes     string  `json:"notes"`
+}
