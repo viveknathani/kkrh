cb9228981308204a3e3918650565d59526958778
viveknathani <viveknathani2402@gmail.com>
2022-01-05 17:24:28 +0530

    chore: clean up for now

--- main.go
+++ main.go
@@ -1,49 +1,4 @@
 package main
 
-import (
-	"io"
-	"log"
-	"net/http"
-	"os"
-
-	"database/sql"
-
-	"github.com/gomodule/redigo/redis"
-	_ "github.com/lib/pq"
-)
-
 func main() {
-
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		io.WriteString(rw, "hello from kkrh-backend")
-	})
-
-	PORT := ""
-	REDIS_URL := ""
-	DATABASE_URL := ""
-
-	if os.Getenv("MODE") == "dev" {
-		PORT = os.Getenv("DEV_PORT")
-		REDIS_URL = os.Getenv("DEV_REDIS_URL")
-		DATABASE_URL = os.Getenv("DEV_DATABASE_URL")
-	} else {
-		PORT = os.Getenv("PORT")
-		REDIS_URL = os.Getenv("REDIS_URL")
-		DATABASE_URL = os.Getenv("DEV_DATABASE_URL")
-	}
-
-	_, err := sql.Open("postgres", DATABASE_URL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = redis.Dial("tcp", REDIS_URL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = http.ListenAndServe(":"+PORT, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
