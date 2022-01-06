e7a243e4b19da58d0bc4bb6ea2282d716c5a69e9
viveknathani <viveknathani2402@gmail.com>
2022-01-06 17:56:51 +0530

    feat: helpers for transactions

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/lib/pq"
 )
@@ -28,4 +29,72 @@ func (db *Database) Initialize(dataSourceName string) error {
 // server wants to shut down.
 func (db *Database) Close() error {
 	return db.pool.Close()
+}
+
+// queryWithTransaction runs the given query within a transaction.
+// The caller can pass a function and form a closure to capture the values
+// from row scanning. The caller should not worry about closing the rows.
+func (db *Database) queryWithTransaction(prepared string, scanRows func(rows *sql.Rows) error, args ...interface{}) error {
+
+	statement, err := db.pool.Prepare(prepared)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+
+	transaction, err := db.pool.Begin()
+	if err != nil {
+		return err
+	}
+
+	rows, err := transaction.Stmt(statement).Query(args...)
+	if err != nil {
+		rollbackError := transaction.Rollback()
+		if rollbackError != nil {
+			log.Println(rollbackError)
+		}
+		return err
+	}
+
+	err = scanRows(rows)
+	if err != nil {
+		return err
+	}
+
+	err = transaction.Commit()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// execWithTransaction will execute a given statement within a transaction.
+func (db *Database) execWithTransaction(prepared string, args ...interface{}) error {
+
+	statement, err := db.pool.Prepare(prepared)
+	if err != nil {
+		return err
+	}
+
+	transaction, err := db.pool.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = transaction.Stmt(statement).Exec(args...)
+	if err != nil {
+		rollError := transaction.Rollback()
+		if rollError != nil {
+			log.Print(rollError)
+		}
+		return err
+	}
+
+	err = transaction.Commit()
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	return nil
 }
