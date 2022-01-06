0ab6029c9fc1264dea537f972b845efe737e9e5c
viveknathani <viveknathani2402@gmail.com>
2022-01-06 17:58:53 +0530

    test: Repository implementation using testing.M

---
+++ database/implementation_test.go
@@ -0,0 +1,146 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/viveknathani/kkrh/entity"
+)
+
+const dsn = "postgres://viveknathani:root@localhost:5432/kkrhdb?sslmode=disable"
+
+var db *Database
+
+func TestMain(t *testing.M) {
+
+	db = &Database{}
+	err := db.Initialize(dsn)
+	code := t.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Close()
+	os.Exit(code)
+}
+
+func TestCreateAndGetUser(t *testing.T) {
+
+	u := &entity.User{
+		Name:     "john",
+		Email:    "john@gmail.com",
+		Password: []byte("someHashedPwd44555"),
+	}
+
+	err := db.CreateUser(u)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, err := db.GetUser(u.Email)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if user == nil {
+		log.Fatal("Got nothing")
+	}
+
+	// since entity.User contains a byte array,
+	// we cannot use the equality operator to test
+	if !reflect.DeepEqual(*u, *user) {
+		log.Println("Inequality.")
+		log.Println("Created: ", u)
+		log.Println("Got: ", user)
+		log.Fatal()
+	}
+
+	// clean up
+	err = db.execWithTransaction("delete from users where name = 'john'")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestCreateAndGetLogs(t *testing.T) {
+
+	u := &entity.User{
+		Name:     "forlogs",
+		Email:    "forlogs@gmail.com",
+		Password: []byte("someHashedPwd44555"),
+	}
+
+	err := db.CreateUser(u)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l := &[]entity.Log{
+		{
+			UserId:    u.Id,
+			Latitude:  78.1,
+			Longitude: 78.2,
+			Activity:  "sleep",
+			StartTime: 45678,
+			EndTime:   45690,
+			Notes:     "",
+		},
+		{
+			UserId:    u.Id,
+			Latitude:  78.1,
+			Longitude: 78.2,
+			Activity:  "work",
+			StartTime: 45678,
+			EndTime:   0,
+		},
+	}
+
+	for _, item := range *l {
+		err := db.CreateLog(&item)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		got, err := db.GetLogs(u.Id, item.EndTime)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if (*got)[0] != item {
+			log.Println(item)
+			log.Println((*got)[0])
+			log.Fatal("Inequality")
+		}
+
+		item.EndTime = 9999
+		err = db.UpdateLog(item.Id, 9999)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		got, err = db.GetLogs(u.Id, item.EndTime)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if (*got)[0] != item {
+			log.Println(item)
+			log.Println((*got)[0])
+			log.Fatal("Inequality")
+		}
+
+		err = db.execWithTransaction(fmt.Sprintf("delete from logs where id='%s'", item.Id))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = db.execWithTransaction("delete from users where name = 'forlogs'")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
