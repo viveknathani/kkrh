a3ab5ec0aad6d27efc6e57b1d9c0d49553505ccb
viveknathani <viveknathani2402@gmail.com>
2022-01-06 17:57:37 +0530

    chore: setup basic API in the interfaces

--- repository/main.go
+++ repository/main.go
@@ -1,12 +1,35 @@
 package repository
 
-// userRepository is the method set to operate on the User model
-type userRepository interface{}
+import "github.com/viveknathani/kkrh/entity"
 
-// logRepository is the method set to operate on the Log model
-type logRepository interface{}
+// userRepository is the method set to operate on the User entity.
+type userRepository interface {
 
-// Repository encapsulates the method set for all models
+	// CreateUser will create a new user in the database and will
+	// have a newly generated UUID.
+	CreateUser(u *entity.User) error
+
+	// GetUser will fetch the first found user from the database.
+	GetUser(email string) (*entity.User, error)
+}
+
+// logRepository is the method set to operate on the Log entity.
+type logRepository interface {
+
+	// CreateLog will create a new log in the database and will have
+	// a newly generated UUID.
+	CreateLog(l *entity.Log) error
+
+	// UpdateLog will update an existing log entry with the new value of
+	// endTime.
+	UpdateLog(logId string, endTime int64) error
+
+	// GetLogs will fetch all logs for the user where stopTime matches the
+	// given endTime.
+	GetLogs(userID string, endTime int64) (*[]entity.Log, error)
+}
+
+// Repository encapsulates the method set for all entities.
 type Repository interface {
 	userRepository
 	logRepository
