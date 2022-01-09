48f5e21342f5567c63d385642c26ce73827e44f1
viveknathani <viveknathani2402@gmail.com>
2022-01-09 15:26:31 +0530

    chore: for /api/log/

--- server/types.go
+++ server/types.go
@@ -1,6 +1,6 @@
 package server
 
-// This file defines how the incoming and outgoing JSON payloads looks like.
+// This file defines how the incoming and outgoing JSON payloads look like.
 
 type genericResponse struct {
 	Message string `json:"message"`
@@ -15,4 +15,17 @@ type userSignupRequest struct {
 type userLoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type logStartRequest struct {
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Activity  string  `json:"activity"`
+	StartTime int64   `json:"startTime"`
+	Notes     string  `json:"notes"`
+}
+
+type logEndRequest struct {
+	LogId   string `json:"logId"`
+	EndTime int64  `json:"endTime"`
 }
